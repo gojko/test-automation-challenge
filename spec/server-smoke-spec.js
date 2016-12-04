d3c1f099e31ece9201079603a5617d89dc335ca7
Gojko Adzic <gojko@gojko.com>
2016-12-04 03:56:07 +0100

    spec cleanup

--- spec/server-smoke-spec.js
+++ spec/server-smoke-spec.js
@@ -1,29 +1,39 @@
 /*global beforeEach, describe, it, console, afterEach */
 
+var Browser = require('zombie');
+
+
 describe('Server smoke test', function () {
 	'use strict';
 	var browser;
+
 	beforeEach(function (done) {
-		browser = new this.Browser();
+		Browser.localhost('example.com', 3000);
+		browser = new Browser();
 		browser.visit('/log-in').then(function () {
-			browser.fill('name', 'admin').fill('password', 'admin').pressButton('#log-in').then(done); //.catch(done);
-		});
+			return browser.fill('name', 'admin')
+				.fill('password', 'admin')
+				.pressButton('#log-in');
+		}).then(done, done.fail);
 	});
 	afterEach(function () {
 		browser.destroy();
 	});
-	it('loggs the admin in', function () {
+	it('logs the admin in', function () {
 		browser.assert.success();
 		console.log('logged in as', browser.text('#login-result-name'));
 		browser.assert.text('#login-result-name', 'admin');
 	});
 	describe('Account creation', function () {
 		beforeEach(function (done) {
-			browser.visit('/util/account').then(done);
+			browser.visit('/util/account').then(done, done.fail);
 		});
 		describe('account balance', function () {
 			beforeEach(function (done) {
-				browser.fill('name', 'gojko').fill('amount', 1000).pressButton('#set-up-account').then(done); //.catch(done);
+				browser.fill('name', 'gojko')
+				.fill('amount', 1000)
+				.pressButton('#set-up-account')
+				.then(done, done.fail);
 			});
 			it('should set balance', function () {
 				browser.assert.success();
@@ -36,7 +46,7 @@ describe('Server smoke test', function () {
 					browser.assert.success();
 					browser.assert.text('#balance', '1000');
 					browser.assert.text('#name', 'gojko');
-				}).catch(this.asyncFail).then(done);
+				}).then(done, done.fail);
 			});
 		});
 	});
