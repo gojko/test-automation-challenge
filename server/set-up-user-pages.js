6c9c536c7715fb52bc3b39adff1e8ec142cf3780
Gojko Adzic <gojko@gojko.com>
2016-12-05 07:56:32 +0100

    topup limits

--- server/set-up-user-pages.js
+++ server/set-up-user-pages.js
@@ -1,6 +1,8 @@
 /*global module */
 module.exports = function setUpUserPages(app, paymentProcessor, accountRepository) {
 	'use strict';
+	var MAX_ACCOUNT_LIMIT = 10000,
+		TX_LIMIT = 5000;
 	app.post('/user/topup', function (req, res) {
 		var name = req.session && req.session.loggedInAccount,
 			card = req.body.card,
@@ -12,6 +14,10 @@ module.exports = function setUpUserPages(app, paymentProcessor, accountRepositor
 			error = 'Please provide a card number';
 		} else if (!amount || isNaN(amount)) {
 			error = 'Amount is invalid';
+		} else if (accountRepository(req)[name] + amount > MAX_ACCOUNT_LIMIT) {
+			error = 'Account limit exceeded';
+		} else if (amount > TX_LIMIT) {
+			error = 'Transaction limit exceeded';
 		} else {
 			paymentResult = paymentProcessor.process({card: card, amount: amount});
 			if (paymentResult.success) {
